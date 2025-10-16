9f020d5d6d7e1c4e0c1381e263b89db24ca9cf44
Phanindra-24 <anusuriphanindra@gmail.com>
2025-10-16 23:49:25 +0530

    operatorexample

---
+++ Test.java
@@ -0,0 +1,39 @@
+package com.codegnan.operatorsexamples;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=10,b=20,c=30;
+		String d = "codegnan";
+		System.out.println(a + b + c + d);//60codegnan
+		System.out.println(b + c + d + a);//50codegnan10
+		System.out.println(c + d + a + b);//30codegnan1020
+		System.out.println(d + a + b + c);//codegnan102030*/
+		
+		/*System.out.println((10<20)&&true);
+		System.out.println(10|2);
+		System.out.println(10^2);*/
+				
+			
+		int i,k;
+		i=10;
+		k=i<0?-i:i;//10
+		System.out.println("absoulte value of "+i+" is "+k);
+		i=-10;
+		k=i<0?-i:i;//10
+		System.out.println("absoulte value of "+i+" is "+k);
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the age");
+		int age= scanner.nextInt();
+		//syntax (condition)?exp1:exp2;
+		String result=(age>18)?"Eligible to vote":"not eligible to vote";
+		System.out.println(result);
+		scanner.close();
+		
+		
+	}
+
+}

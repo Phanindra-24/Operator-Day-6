17ff41d68e23435ce27a68b47753edd7e1f8218a
Phanindra-24 <anusuriphanindra@gmail.com>
2025-10-22 00:43:33 +0530

    Add files via upload

---
+++ EvenOddCheck.java
@@ -0,0 +1,22 @@
+package com.codegnan.operatorsexamples;
+
+import java.util.Scanner;
+
+public class EvenOddCheck {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int number = scanner.nextInt();
+		if (number % 2 == 0) {
+			System.out.println("The number is even:");
+		} else {
+				System.out.println("The number is odd:");
+			}
+		scanner.close();
+		
+		}
+
+	}
+
+
